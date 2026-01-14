727257ef616bb3a4bc252407af0a6d359afc138f
脆皮五花肉 <danielchenwu666@gmail.com>
2026-01-14 03:28:33 +0000

    初始化项目

---
+++ server.js
@@ -0,0 +1,34 @@
+import { createRequestHandler } from "@remix-run/express";
+import express from "express";
+
+const port = process.env.SERVER_PORT || 3000
+
+// 热更新
+const viteDevServer =
+  process.env.NODE_ENV === "production"
+    ? null
+    : await import("vite").then((vite) =>
+        vite.createServer({
+          server: { middlewareMode: true },
+        })
+      );
+
+const app = express();
+app.use(
+  viteDevServer
+    ? viteDevServer.middlewares
+    : express.static("build/client")
+);
+
+const build = viteDevServer
+  ? () =>
+      viteDevServer.ssrLoadModule(
+        "virtual:remix/server-build"
+      )
+  : await import("./build/server/index.js");
+
+app.all("*", createRequestHandler({ build }));
+
+app.listen(port, () => {
+  console.log(`App listening on http://localhost:${port}`);
+});
